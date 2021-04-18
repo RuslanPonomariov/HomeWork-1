15846ae6fe553bcb904a746443de41333cbb4731
Ruslan <ruslanfeya5@gmail.com>
2021-04-18 13:33:27 +0500

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Добро пожаловать в консоль!');
+
+
+var Name = prompt ('Введите в поле ниже вашe имя.');
+console.log(Name);
+
+
+var age = prompt ('Введите в поле ниже ваш возраст.');
+console.log(age);
+
+
+var examples = prompt ('Сколько будет 234+673?');
+console.log('234 + 673 = 907' + ' Правиьный ответ: 907');~
+console.log('Ваш ответ:'); console.log(examples);
+
